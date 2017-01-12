b587e5b36fa9474c1a1a3ea0a4db193d5e6ed2f8
AdiPerescu <adrianperescu17@gmail.com>
2017-01-12 23:52:56 +0200

    Portfolio design

---
+++ assets/js/functions.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+  smoothScroll(500)
+
+});
+
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
